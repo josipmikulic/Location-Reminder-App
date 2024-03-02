261f5dfc93ce3a188e43f3b438a5d311e61f2c17
josipmikulic <rofljosipmikulic@gmail.com>
2024-03-02 09:24:41 +0100

    * Setup project * Add firebase auth login and logout * Add google maps api key

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,17 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,15 +19,76 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
-        // TODO: Implement the create account and sign in using FirebaseUI,
-        //  use sign in using email and sign in using Google
+        loginButton = findViewById<Button>(R.id.button_login).apply {
+            isEnabled = false
+            setOnClickListener {
+                startSignIn()
+            }
+        }
 
-        // TODO: If the user was authenticated, send him to RemindersActivity
+        checkIfUserLoggedIn()
+    }
 
-        // TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+    private fun checkIfUserLoggedIn() {
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            navigateToRemindersActivity()
+        } else {
+            loginButton.isEnabled = true
+        }
+    }
+
+    private fun startSignIn() {
+        val signInIntent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+            listOf(
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+                AuthUI.IdpConfig.EmailBuilder().build(),
+            )
+        ).setTheme(R.style.AppTheme).build()
+
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun navigateToRemindersActivity() {
+        val intent = Intent(
+            this,
+            RemindersActivity::class.java
+        ).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        startActivity(intent)
+    }
+
+    private val signInLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+
+    ) { result ->
+        val response = result.idpResponse
+
+        if (result.resultCode == RESULT_OK) {
+            navigateToRemindersActivity()
+        } else {
+            var errorText = if (response == null) {
+                getString(R.string.error_sign_in_cancelled)
+            } else {
+                if (response.error?.errorCode == ErrorCodes.NO_NETWORK) {
+                    getString(R.string.error_no_internet)
+                } else {
+                    getString(R.string.error_unknown)
+                }
+            }
+
+            Toast.makeText(
+                this,
+                errorText,
+                Toast.LENGTH_SHORT
+            )
+        }
     }
 }
